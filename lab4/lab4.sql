a557d4e967649d670929424deee27941f19d5f26
justinesposito <espo289@hotmail.com>
2014-02-14 13:49:38 -0500

    Lab 4

---
+++ lab4/lab4.sql
@@ -0,0 +1,71 @@
+﻿
+--Justin Esposito - Lab 4
+
+--Q1
+select city
+from agents
+where aid in (select aid
+		from orders
+		where cid in (select cid
+				from customers
+				where name = 'Basics'
+				)
+		)
+
+--Q2
+select pid
+from orders
+where aid in (select aid
+		from customers
+		where cid in ( select cid
+				from customers
+				where city = 'Kyoto'
+		)		)
+
+--Q3
+select cid, name
+from customers
+where cid in (select cid
+		from orders
+		where aid != 'a03'
+		)
+
+--Q4
+select cid, name
+from customers
+where cid in (select cid
+		from orders
+		where pid in ('p01', 'p07')
+		group by cid
+		having count(pid) > 1
+		)
+		
+
+--Q5
+select pid
+from orders
+where cid in (select cid
+		from orders
+		where aid = 'a03'
+		)
+
+--Q6
+select name, discount
+from customers
+where cid in (select cid
+		from orders
+		where aid in ( select aid
+				from agents
+				where city = 'Dallas' or city = 'Duluth'
+				)
+		)
+
+--Q7
+select *
+from customers
+where city != 'Kyoto'
+and city != 'Dallas'
+and discount in (select discount
+			from customers
+			where city in ('Dallas', 'Kyoto')
+			)
